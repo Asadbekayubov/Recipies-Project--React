1deaeb87ed1fc519622e3e7e26c7b48ee74fa70e
Asadulloh <ayubovasadbek33@gmail.com>
2023-10-10 20:28:05 +0500

    finished

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -22,11 +22,11 @@ function Recipe() {
         <div className="card-body">
           <h2 className="card-title text-5xl mb-2">{title}</h2>
           <h3 className="text-2xl">Ingredients:</h3>
-          <ul className="flex gap-6 flex-wrap items-center justify-center">
+          <ul className="flex flex-wrap items-center justify-center">
             {ingredients &&
               ingredients.map((gradient) => {
                 return (
-                  <li key={gradient} className=" text-center">
+                  <li key={gradient} className="mx-2 text-center">
                     <p className="text-1xl">{gradient}</p>
                   </li>
                 );
